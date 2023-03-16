c80ce6803777771987a8f83823ddb5b7a1bf17b7
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-03-16 00:04:52 -0300

    Implementação do SecurityFilter.java

---
+++ src/main/java/com/pieropan/networksocial/secutiry/SecurityFilter.java
@@ -0,0 +1,40 @@
+package com.pieropan.networksocial.secutiry;
+
+import com.pieropan.networksocial.repository.UsersRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UsersRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+                                    throws ServletException, IOException {
+
+        var token = request.getHeader("Authorization");
+        if(Objects.nonNull(token)){
+            var subject = tokenService.getSubject(token);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/pieropan/networksocial/secutiry/TokenService.java
+++ src/main/java/com/pieropan/networksocial/secutiry/TokenService.java
@@ -3,6 +3,7 @@ package com.pieropan.networksocial.secutiry;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.pieropan.networksocial.domain.Users;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,19 +18,31 @@ public class TokenService {
     @Value("${token.secret}")
     String secret;
 
-    public String generateToken(Users user){
+    public String generateToken(Users user) {
         try {
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withSubject(user.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
             throw new RuntimeException("erro ao gerrar token jwt", exception);
         }
     }
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String token) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
     }
 }

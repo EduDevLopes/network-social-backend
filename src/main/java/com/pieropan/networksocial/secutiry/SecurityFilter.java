e3763032e21d5251c5f76bb63198c0d76007d451
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-03-16 20:26:24 -0300

    Configurando o  cors do meu servidor. Todos endpoints, todas as origens e todos os métodos http.

--- src/main/java/com/pieropan/networksocial/secutiry/SecurityFilter.java
+++ src/main/java/com/pieropan/networksocial/secutiry/SecurityFilter.java
@@ -35,7 +35,6 @@ public class SecurityFilter extends OncePerRequestFilter {
             var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        response.setHeader("Access-Control-Allow-Origin", "*");
         filterChain.doFilter(request, response);
     }
 }

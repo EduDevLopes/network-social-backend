00b733676f02ed70720ed1e16b632bec3671ce5c
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-03-15 21:04:37 -0300

    Primeiro commit

---
+++ src/main/java/com/pieropan/networksocial/domain/User.java
@@ -0,0 +1,19 @@
+package com.pieropan.networksocial.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/test/java/com/pieropan/networksocial/NetworkSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pieropan.networksocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

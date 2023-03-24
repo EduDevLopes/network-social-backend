f51cc7aa6517459e0054e645ff90d92a38c0f7ea
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-03-24 17:55:05 -0300

    Melhoria no arquivo de configuração

--- src/main/java/com/pieropan/networksocial/http/MicroServiceEmail.java
+++ src/main/java/com/pieropan/networksocial/http/MicroServiceEmail.java
@@ -2,13 +2,12 @@ package com.pieropan.networksocial.http;
 
 import com.pieropan.networksocial.dto.EmailDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "ms-email", url = "http://localhost:8081/email")
+@FeignClient(name = "ms-email", url = "${spring.service.mail}")
 public interface MicroServiceEmail {
 
     @RequestMapping(method = RequestMethod.POST, value = "/send")
-    ResponseEntity send(EmailDto emailDto);
+    void send(EmailDto emailDto);
 }

1d5d89e5d2ca70f2009aecaefe5357edc10f79bd
PIEROPAN <matheus.pieropan@viannasempre.com.br>
2023-03-27 13:40:15 -0300

    Ajuste na ordenação do enum!

--- src/main/java/com/pieropan/networksocial/enums/SchoolingEnum.java
+++ src/main/java/com/pieropan/networksocial/enums/SchoolingEnum.java
@@ -4,12 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum SchoolingEnum {
-    ENSINO_MEDIO_COMPLETO("Ensino Médio Completo"),
     ENSINO_MEDIO_INCOMPLETO("Ensino Médio Incompleto"),
-    ENSINO_SUPERIOR_COMPLETO("Ensino Superior Completo"),
+    ENSINO_MEDIO_COMPLETO("Ensino Médio Completo"),
     ENSINO_SUPERIOR_INCOMPLETO("Ensino Superior Incompleto"),
-    POS_GRADUCAO_INCOMPLETA("Pós-Graduação Completa"),
-    POS_GRADUCAO_COMPLETA("Pós-Graduação Incompleta");
+    ENSINO_SUPERIOR_COMPLETO("Ensino Superior Completo"),
+    POS_GRADUCAO_COMPLETA("Pós-Graduação Incompleta"),
+    POS_GRADUCAO_INCOMPLETA("Pós-Graduação Completa");
 
     private String name;
 
